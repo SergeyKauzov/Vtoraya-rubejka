beba973aa1efbb138257d2aad10313b8ff5d5b1a
SergeyKauzov <kauzov02@mail.ru>
2020-12-02 13:27:02 +0500

    class

---
+++ class/main.py
@@ -0,0 +1,20 @@
+class stydent():
+     def __init__(this, name, surname,age,group,course,):
+         this.name = name
+         this.surname = surname
+         this.age = age
+         this.group = group
+         this.course = course
+
+
+stydent1 = stydent("Сергей"," Каузов","18","АиУп-19р/о","2")
+stydent2 = stydent("Парвиз","Болатов ","18","АиУп-19р/о","2")
+stydent3 = stydent("Никита","Иванов ","18","АиУп-19р/о","2")
+stydent4 = stydent("Асем","Серикова ","19","АиУп-19р/о","2")
+stydent5 = stydent("Дильназ","Кубашева ","18","АиУп-19р/о","2")
+
+print(stydent1.name,stydent1.surname,stydent1.age,stydent1.group,stydent1.course)
+print(stydent2.name,stydent2.surname,stydent2.age,stydent2.group,stydent2.course)
+print(stydent3.name,stydent3.surname,stydent3.age,stydent3.group,stydent3.course)
+print(stydent4.name,stydent4.surname,stydent4.age,stydent4.group,stydent4.course)
+print(stydent5.name,stydent5.surname,stydent5.age,stydent5.group,stydent5.course)
