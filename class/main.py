767e743a99c323e1d7a25f8eb80bdf7116106d61
SergeyKauzov <kauzov02@mail.ru>
2020-12-04 17:51:15 +0500

    i

--- class/main.py
+++ class/main.py
@@ -7,14 +7,15 @@ class stydent():
          this.course = course
 
 
-stydent1 = stydent("Сергей"," Каузов","18-лет","АиУп-19р/о","Курс-2")
-stydent2 = stydent("Парвиз","Болатов ","18-лет","АиУп-19р/о","Курс-2")
-stydent3 = stydent("Никита","Иванов ","18-лет","АиУп-19р/о","Курс-2")
-stydent4 = stydent("Асем","Серикова ","19-лет","АиУп-19р/о","Курс-2")
-stydent5 = stydent("Дильназ","Кубашева ","18-лет","АиУп-19р/о","Курс-2")
+stydent1 = stydent("Сергей"," Каузов",18,"АиУп-19р/о","Курс-2")
+stydent2 = stydent("Парвиз","Болатов ",18,"АиУп-19р/о","Курс-2")
+stydent3 = stydent("Никита","Иванов ",18,"АиУп-19р/о","Курс-2")
+stydent4 = stydent("Асем","Серикова ",19,"АиУп-19р/о","Курс-2")
+stydent5 = stydent("Дильназ","Кубашева ",18,"АиУп-19р/о","Курс-2")
 
 print(stydent1.name,stydent1.surname,stydent1.age,stydent1.group,stydent1.course)
 print(stydent2.name,stydent2.surname,stydent2.age,stydent2.group,stydent2.course)
 print(stydent3.name,stydent3.surname,stydent3.age,stydent3.group,stydent3.course)
 print(stydent4.name,stydent4.surname,stydent4.age,stydent4.group,stydent4.course)
 print(stydent5.name,stydent5.surname,stydent5.age,stydent5.group,stydent5.course)
+print(type(stydent1.age))
